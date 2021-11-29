43737fb3c1107e3c34aa6c6fcb61b60f147ec3b4
Leim <la.blake107@gmail.com>
2021-11-29 16:47:24 +0100

    Added functions for numbers and Delete/Undo

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const screen = document.querySelector('.screen');
-////////////////////////////////////////////////////////
+const allNumbers = document.querySelectorAll('.number');
+let n = 0;
+let virgNum = 0;
+
+function raiseToNine() {
+    if (n == 9) {
+        allNumbers.forEach(number => number.disabled = true);
+        virgule.disabled = true;
+    }
+}
+function max1Virg() {
+    if (virgNum == 1) {
+        virgule.disabled = true;
+    }
+}
+
 function no1() {
     const write1 = document.createTextNode("1");
     screen.appendChild(write1);
 }
 const un = document.getElementById('un');
 un.onclick = () => {
-    no1()
+    no1();
+    n++;
+    raiseToNine();
 };
 
 function no2() {
@@ -15,7 +32,9 @@ function no2() {
 }
 const deux = document.getElementById('deux');
 deux.onclick = () => {
-    no2()
+    no2();
+    n++;
+    raiseToNine();
 };
 
 function no3() {
@@ -24,7 +43,9 @@ function no3() {
 }
 const trois = document.getElementById('trois');
 trois.onclick = () => {
-    no3()
+    no3();
+    n++;
+    raiseToNine();
 };
 
 function no4() {
@@ -33,7 +54,9 @@ function no4() {
 }
 const quatre = document.getElementById('quatre');
 quatre.onclick = () => {
-    no4()
+    no4();
+    n++;
+    raiseToNine();
 };
 
 function no5() {
@@ -42,7 +65,9 @@ function no5() {
 }
 const cinq = document.getElementById('cinq');
 cinq.onclick = () => {
-    no5()
+    no5();
+    n++;
+    raiseToNine();
 };
 
 function no6() {
@@ -51,7 +76,9 @@ function no6() {
 }
 const six = document.getElementById('six');
 six.onclick = () => {
-    no6()
+    no6();
+    n++;
+    raiseToNine();
 };
 
 function no7() {
@@ -60,7 +87,9 @@ function no7() {
 }
 const sept = document.getElementById('sept');
 sept.onclick = () => {
-    no7()
+    no7();
+    n++;
+    raiseToNine();
 };
 
 function no8() {
@@ -69,7 +98,9 @@ function no8() {
 }
 const huit = document.getElementById('huit');
 huit.onclick = () => {
-    no8()
+    no8();
+    n++;
+    raiseToNine();
 };
 
 function no9() {
@@ -78,7 +109,9 @@ function no9() {
 }
 const neuf = document.getElementById('neuf');
 neuf.onclick = () => {
-    no9()
+    no9();
+    n++;
+    raiseToNine();
 };
 
 function no0() {
@@ -87,14 +120,46 @@ function no0() {
 }
 const zéro = document.getElementById('zéro');
 zéro.onclick = () => {
-    no0()
+    no0();
+    n++;
+    raiseToNine();
 };
-////////////////////////////////////////////////////////
+
+function virg() {
+    const writeVirg = document.createTextNode(",");
+    screen.appendChild(writeVirg);
+}
+const virgule = document.getElementById('virg');
+virgule.onclick = () => {
+    virg();
+    virgNum++;
+    max1Virg();
+};
 
 
 
-////////////////////////////////////////////////////////
 const eff = document.getElementById('effacer');
 eff.onclick = () => {
     screen.textContent = "";
+    n = 0;
+    virgNum = 0;
+    allNumbers.forEach(number => number.disabled = false);
+    virgule.disabled = false;
+}
+const ann = document.getElementById('annuler');
+ann.onclick = () => {
+    screen.textContent = screen.textContent.substring(0, screen.textContent.length - 1);
+    n--; // MODIFICA: SE IO CANCELLO LA VIRGOLA, MI TOGLIE CMQ 1 PUNTO A n!!! DA FARE ANK: SE METTI VIRGOLA, DI CIFRE CE NE STANNO SOLO 8 POI!!!
+    if (n < 9) {
+        allNumbers.forEach(number => number.disabled = false);
+    }
+    let decimals = screen.textContent.search(",");
+    if (decimals !== -1) {
+        virgule.disabled = true;
+    }
+    else if (decimals == -1) {
+        virgNum = 0;
+        virgule.disabled = false;
+        max1Virg();
+    }
 }
