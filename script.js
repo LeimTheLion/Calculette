5b6e2a555717705a6b6bf36030aec7d3c16935af
Leim <la.blake107@gmail.com>
2021-11-29 11:28:35 +0100

    Added hover effect

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.querySelectorAll('button');
+for (let i = 0; i < 21; i++) {
+    
+    //button[i].addEventListener("mouseover", mouseOver);
+    //function mouseOver(e) {
+      //  e.target.style.backgroundColor = "gray";
+        //e.target.style.backgroundColor.opacity = "0.2";
+       // }
+    //button[i].addEventListener("mouseout", mouseOut);
+    //function mouseOut(e) {
+      //  e.target.style.backgroundColor = "";
+    //}
+}
