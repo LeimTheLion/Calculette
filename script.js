a03d20a7bca220323239d5a59b29c9e5991b7168
Leim <la.blake107@gmail.com>
2021-11-29 12:49:34 +0100

    Now working on js

--- script.js
+++ script.js
@@ -1,13 +1,100 @@
-const button = document.querySelectorAll('button');
-for (let i = 0; i < 21; i++) {
-    
-    //button[i].addEventListener("mouseover", mouseOver);
-    //function mouseOver(e) {
-      //  e.target.style.backgroundColor = "gray";
-        //e.target.style.backgroundColor.opacity = "0.2";
-       // }
-    //button[i].addEventListener("mouseout", mouseOut);
-    //function mouseOut(e) {
-      //  e.target.style.backgroundColor = "";
-    //}
+const screen = document.querySelector('.screen');
+////////////////////////////////////////////////////////
+function no1() {
+    const write1 = document.createTextNode("1");
+    screen.appendChild(write1);
+}
+const un = document.getElementById('un');
+un.onclick = () => {
+    no1()
+};
+
+function no2() {
+    const write2 = document.createTextNode("2");
+    screen.appendChild(write2);
+}
+const deux = document.getElementById('deux');
+deux.onclick = () => {
+    no2()
+};
+
+function no3() {
+    const write3 = document.createTextNode("3");
+    screen.appendChild(write3);
+}
+const trois = document.getElementById('trois');
+trois.onclick = () => {
+    no3()
+};
+
+function no4() {
+    const write4 = document.createTextNode("4");
+    screen.appendChild(write4);
+}
+const quatre = document.getElementById('quatre');
+quatre.onclick = () => {
+    no4()
+};
+
+function no5() {
+    const write5 = document.createTextNode("5");
+    screen.appendChild(write5);
+}
+const cinq = document.getElementById('cinq');
+cinq.onclick = () => {
+    no5()
+};
+
+function no6() {
+    const write6 = document.createTextNode("6");
+    screen.appendChild(write6);
+}
+const six = document.getElementById('six');
+six.onclick = () => {
+    no6()
+};
+
+function no7() {
+    const write7 = document.createTextNode("7");
+    screen.appendChild(write7);
+}
+const sept = document.getElementById('sept');
+sept.onclick = () => {
+    no7()
+};
+
+function no8() {
+    const write8 = document.createTextNode("8");
+    screen.appendChild(write8);
+}
+const huit = document.getElementById('huit');
+huit.onclick = () => {
+    no8()
+};
+
+function no9() {
+    const write9 = document.createTextNode("9");
+    screen.appendChild(write9);
+}
+const neuf = document.getElementById('neuf');
+neuf.onclick = () => {
+    no9()
+};
+
+function no0() {
+    const write0 = document.createTextNode("0");
+    screen.appendChild(write0);
+}
+const zéro = document.getElementById('zéro');
+zéro.onclick = () => {
+    no0()
+};
+////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////
+const eff = document.getElementById('effacer');
+eff.onclick = () => {
+    screen.textContent = "";
 }
